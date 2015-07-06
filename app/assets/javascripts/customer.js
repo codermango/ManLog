8351b4d0bdc764045a0c39589d7c9419e5b725d5
codermango <codermango@gmail.com>
2015-07-07 00:33:17 +0200

    Change the implementation of tabs in admin page

--- app/assets/javascripts/customer.js
+++ app/assets/javascripts/customer.js
@@ -1,16 +1,7 @@
 $(function() {
-    $("#write-article").hide();
-    $("#li-manage-articles").click(function() {
-        $("#li-write-article").removeClass("active");
-        $("#li-manage-articles").addClass("active");
-        $("#manage-articles").show();
-        $("#write-article").hide();
-    });
-    $("#li-write-article").click(function() {
-        $("#li-write-article").addClass("active");
-        $("#li-manage-articles").removeClass("active");
-        $("#manage-articles").hide();
-        $("#write-article").show();
+    $('#adminTab a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
     });
 
 
